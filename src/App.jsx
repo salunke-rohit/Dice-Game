cb9b6012d848e260a0aca7d3b5cf43c015f7c084
RohitSalunke <rohitsalunke107@gmail.com>
2026-02-23 22:21:33 +0530

    styled componets are installed and home page is start to create

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled from "styled-components"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
